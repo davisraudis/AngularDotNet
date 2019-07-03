abd02b4fd3d9ba44cd4cfacedba13946a68a4e25
Davis-PC\Davis <dafels94@inbox.lv>
2019-07-03 23:13:20 +0300

    2. uzdevums

--- majasdarbs/src/app/app.module.ts
+++ majasdarbs/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatMenuModule, MatIconModule, MatCardModule, MatButtonModule, MatButton } from '@angular/material';
+import { MatToolbarModule, MatMenuModule, MatIconModule, MatCardModule, MatButtonModule, MatProgressSpinnerModule, MatTableModule, MatSnackBarModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ParagraphDisplayComponent } from './components/paragraph-display/paragraph-display.component';
 import { ParagraphCardComponent } from './components/paragraph-card/paragraph-card.component';
+import { ExchangeRatesDisplayComponent } from './components/exchange-rates-display/exchange-rates-display.component';
 
+import { ExchangeService } from './shared/exchange.service'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ParagraphCardComponent,
-    ParagraphDisplayComponent
+    ParagraphDisplayComponent,
+    ExchangeRatesDisplayComponent
   ],
   imports: [
     BrowserModule,
@@ -27,9 +32,14 @@ import { ParagraphCardComponent } from './components/paragraph-card/paragraph-ca
     MatCardModule,
     FlexLayoutModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatSnackBarModule,
+    NoopAnimationsModule
   ],
-  providers: [],
+  providers: [ExchangeService],
   bootstrap: [
     AppComponent
   ]
